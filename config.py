5b0bb9dfc43186a795069e7778a0f5ce308a2140
Telgrambot <faizandiwan4690@gmail.com>
2024-07-29 12:03:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,17 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7428651487:AAGLh_fd5vWk-n0vdmJ1c0qxpYitIuQ_ZNY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("BEEDIBABABOT")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1803355945")
 
 
 
 
 
 
-
-  
+   
